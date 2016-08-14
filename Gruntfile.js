cd7b26ea0bc7bfd9283ba2080d24e1026b75f2e4
Fabian Damken <fabian.damken@gmx.de>
2016-08-14 14:01:32 +0200

    Configured coveralls not to fail.

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,6 +69,9 @@ module.exports = function (grunt) {
             }
         },
         coveralls: {
+            options: {
+                force: true
+            },
             upload: {
                 src: 'coverage/lcov.info'
             }
